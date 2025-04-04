f27a43f267f524b3cc3b63f750b4a615bd01bfed
Ehsan <eh.etesami@gmail.com>
2025-04-04 00:56:37 +0000

    Update utils calculate RTT function

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -4,24 +4,30 @@ import (
 	"fmt"
 	"strconv"
 	"time"
-
-	pb "github.com/etesami/air-quality-monitoring/pkg/protoc"
 )
+
+// calculateRtt calculates the round-trip time (RTT) based on the current time and the ack time
+func CalculateRtt(msgSentTime time.Time, msgRecTime string, ackRecTime time.Time, ackSentTime string) (float64, error) {
+	msgRecTime1, err1 := StrUnixToTime(msgRecTime)
+	ackSentTime1, err2 := StrUnixToTime(ackSentTime)
+	if err1 != nil || err2 != nil {
+		return -1, fmt.Errorf("error parsing timestamps: %v, %v", err1, err2)
+	}
+	t1 := msgRecTime1.Sub(msgSentTime)
+	t2 := ackRecTime.Sub(ackSentTime1)
+	rtt := float64(t1+t2) / 1000.0
+	return rtt, nil
+}
+
+func StrUnixToTime(unixStr string) (time.Time, error) {
+	unixInt, err := strconv.ParseInt(unixStr, 10, 64)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("failed to parse unix time: %v", err)
+	}
+	return UnixMilliToTime(unixInt), nil
+}
 
 // UnixMilliToTime converts a Unix timestamp in milliseconds to a time.Time object
 func UnixMilliToTime(unixMilli int64) time.Time {
 	return time.Unix(unixMilli/1000, (unixMilli%1000)*int64(time.Millisecond))
-}
-
-// calculateRtt calculates the round-trip time (RTT) based on the current time and the ack time
-// It takes the sent message time, received ack time, and the ack object as parameters
-func CalculateRtt(sentMsgTime, recAckTime time.Time, ack pb.Ack) (float64, error) {
-	sentAckTime, err1 := strconv.Atoi(ack.AckSentTimestamp)
-	recMsgTime, err2 := strconv.Atoi(ack.ReceivedTimestamp)
-	if err1 != nil || err2 != nil {
-		return 0, fmt.Errorf("error converting ack timestamps: %v, %v", err1, err2)
-	}
-	t1 := int64(recMsgTime) - sentMsgTime.UnixMilli()
-	t2 := recAckTime.UnixMilli() - int64(sentAckTime)
-	return float64(t1+t2) / 1000.0, nil
 }
