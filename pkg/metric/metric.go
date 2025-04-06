84c6a4e235ee6f984e10e331894bc9f89433b91f
Ehsan <eh.etesami@gmail.com>
2025-04-06 20:45:37 +0000

    Add prometheus Gauge metrics

--- pkg/metric/metric.go
+++ pkg/metric/metric.go
@@ -9,26 +9,40 @@ import (
 var (
 	// Buckets for processing histogram in milliseconds
 	// customBuckets = []float64{0.01, 0.1, 0.5, 1, 2, 5, 10, 20, 50, 100}
-	processingTime = prometheus.NewHistogram(
+	procTimeHistogram = prometheus.NewHistogram(
 		prometheus.HistogramOpts{
-			Name:    "operation_processing_time_seconds",
+			Name:    "processing_time_ms_histogram",
 			Help:    "Histogram of processing times.",
 			Buckets: prometheus.DefBuckets,
 		},
 	)
-	rttTime = prometheus.NewHistogramVec(
+	rttTimeHistogram = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:    "operation_rtt_time_seconds",
+			Name:    "rtt_times_ms_histogram",
 			Help:    "Histogram of round-trip times for different services.",
 			Buckets: prometheus.DefBuckets,
 		},
 		[]string{"service"},
 	)
+	procTime = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "processing_time_ms",
+			Help: "Gauge of processing times.",
+		},
+	)
+	rTTTimes = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "rtt_times_ms",
+			Help: "Gauge of round-trip times for different services.",
+		},
+		[]string{"service"})
 )
 
 func (m *Metric) RegisterMetrics() {
-	prometheus.MustRegister(processingTime)
-	prometheus.MustRegister(rttTime)
+	prometheus.MustRegister(procTimeHistogram)
+	prometheus.MustRegister(rttTimeHistogram)
+	prometheus.MustRegister(procTime)
+	prometheus.MustRegister(rTTTimes)
 }
 
 type Metric struct {
@@ -38,13 +52,15 @@ type Metric struct {
 func (m *Metric) AddProcessingTime(s string, time float64) {
 	m.lock()
 	defer m.unlock()
-	processingTime.Observe(time)
+	procTimeHistogram.Observe(time)
+	procTime.Set(time)
 }
 
 func (m *Metric) AddRttTime(s string, time float64) {
 	m.lock()
 	defer m.unlock()
-	rttTime.WithLabelValues(s).Observe(time)
+	rttTimeHistogram.WithLabelValues(s).Observe(time)
+	rTTTimes.WithLabelValues(s).Set(time)
 }
 
 func (m *Metric) lock() {
