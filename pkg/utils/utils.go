07e8fc6946c73b3530d3738cf440b3e2cff4dd01
Ehsan <eh.etesami@gmail.com>
2025-04-25 01:10:00 +0000

    Add (utils) parse bucket functions

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -31,4 +32,23 @@ func StrUnixToTime(unixStr string) (time.Time, error) {
 // UnixMilliToTime converts a Unix timestamp in milliseconds to a time.Time object
 func UnixMilliToTime(unixMilli int64) time.Time {
 	return time.Unix(unixMilli/1000, (unixMilli%1000)*int64(time.Millisecond))
+}
+
+// ParseBuckets parses a comma-separated string of bucket values into a slice of float64
+func ParseBuckets(env string) []float64 {
+	if env == "" {
+		return nil
+	}
+	parts := strings.Split(env, ",")
+	var buckets []float64
+	for _, p := range parts {
+		if f, err := strconv.ParseFloat(strings.TrimSpace(p), 64); err == nil {
+			buckets = append(buckets, f)
+		} else {
+			// print error
+			fmt.Printf("Error parsing bucket value '%s': %v\n", p, err)
+			return nil
+		}
+	}
+	return buckets
 }
